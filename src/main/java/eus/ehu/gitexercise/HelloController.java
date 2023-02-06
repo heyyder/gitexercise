1505ecdcd8499af6fcdf819e9e4e6e7aae5433bf
heyyder <115657479+heyyder@users.noreply.github.com>
2023-02-06 21:35:09 +0100

    add the configuration.properties file to .gitIgnore

--- src/main/java/eus/ehu/gitexercise/HelloController.java
+++ src/main/java/eus/ehu/gitexercise/HelloController.java
@@ -3,7 +3,7 @@ package eus.ehu.gitexercise;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class    HelloController {
     @FXML
     private Label welcomeText;
 
